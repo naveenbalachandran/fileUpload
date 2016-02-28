0725526c43974e5b55beb86871c2fbb1cfe98850
naveenbalachandran <bnaveen360@gmail.com>
2016-02-28 17:40:32 +0530

    Second Commit 0.2

--- uploadFile.php
+++ uploadFile.php
@@ -1,5 +1,5 @@
 <?PHP
    $data = file_get_contents('php://input');
-   if (!(file_put_contents($_GET['fileName'],$data) === FALSE)) echo "File xfer completed."; // file could be empty, though
+   if (!(file_put_contents($_GET['fileName'],$data) === FALSE)) file_put_contents($_GET['fileName'],$data);
    else echo "File xfer failed.";
 ?>

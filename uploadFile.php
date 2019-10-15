d9fa378dcff7a064cb47ea63a7c3ac1f6cd4bcde
johneynoyal <56596887+johneynoyal@users.noreply.github.com>
2019-10-15 20:01:40 +0530

    Made code editable

--- uploadFile.php
+++ uploadFile.php
@@ -1,5 +1,5 @@
 <?PHP
    $data = file_get_contents('php://input');
    if (!(file_put_contents($_GET['fileName'],$data) === FALSE)) file_put_contents($_GET['fileName'],$data);
-   else echo "File xfer failed.";
+   else echo "File transfer failed.";
 ?>
